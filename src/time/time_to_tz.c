92532fff78797e36bb01c6694aefbcd01c1779eb
Markus Wichmann <nullplan@gmx.net>
2025-01-10 10:49:33 +0100

    Add POSIX time parser.
    It is not quite correct. The algorithm finds the DST transition times
    for the given year and works with that. Unfortunately, if the time is
    given as local time, then I cannot reliably know the correct year. So if
    a DST change happens right at the change of year (or within the DST
    difference) I may evaluate the wrong time zone. I don't really know how
    to fix it.
    
    I still need a TZ file parser, but I have now also added the code that
    finds the file.

--- src/time/time_to_tz.c
+++ src/time/time_to_tz.c
@@ -6,11 +6,12 @@
 #include <pthread.h>
 #include <limits.h>
 #include <stdint.h>
+#include <ctype.h>
 #include "syscall.h"
 #include "libc.h"
 
 static struct lock lock;
-static char tzbuf[2][TZNAME_MAX];
+static char tzbuf[2][TZNAME_MAX + 1];
 static char *__tzname[2];
 static int __daylight;
 static long __timezone;
@@ -20,10 +21,148 @@ static const void *infofile;
 static const size_t infosize;
 static const uint64_t *transitions;
 static const char *abbrevs;
+static long long maxtime;
+struct rule {
+    enum { GREGORIAN, JULIAN, WEEKDAY } type;
+    union {
+        long day;
+        unsigned char para[3];
+    } u;
+    long time;
+};
+static struct rule rules[2];
 
 weak_alias(tzname, __tzname);
 weak_alias(daylight, __daylight);
 weak_alias(timezone, __timezone);
+
+static const char *getname(const char *s, char *buf, size_t len)
+{
+    if (*s == '<') {
+        const char *z = strchr(s, '>');
+        if (z && z - s - 1 < len) {
+            memcpy(buf, s + 1, z - s - 1);
+            buf[z - s - 1] = 0;
+            return z + 1;
+        }
+    } else {
+        size_t spn = strspn(s, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+        if (spn >= 3 && spn < len) {
+            memcpy(buf, s, spn);
+            return s + spn;
+        }
+    }
+    return 0;
+}
+
+static const char *getint(const char *s, long *val)
+{
+    char *endp;
+    *val = strtol(s, &endp, val);
+    return endp == s? 0 : endp;
+}
+
+static const char *getoff(const char *s, long *secs)
+{
+    long hours, minutes, seconds;
+    minutes = seconds = 0;
+    s = getint(s, &hours);
+    if (!s) return 0;
+    if (*s == ':') {
+        s = getint(s + 1, &minutes);
+        if (!s) return 0;
+        if (*s == ':') {
+            s = getint(s + 1, &seconds);
+            if (!s) return 0;
+        }
+    }
+    if (hours - LONG_MIN/3600 + 0ul >= ULONG_MAX/3600 || minutes + 0ul >= 60ul || seconds + 0ul >= 60ul)
+        return 0;
+    *secs = hours * 3600 + minutes * 60 + seconds;
+    return s;
+}
+
+static const char *getrule(const char *s, struct rule *r)
+{
+    char *endp;
+    if (isdigit(*s)) {
+        r->type = GREGORIAN;
+        r->u.day = strtol(s, &endp, 10);
+    } else if (*s == 'J' && isdigit(s[1])) {
+        r->type = JULIAN;
+        r->u.day = strtol(s + 1, &endp, 10);
+    } else if (*s == 'M'
+            && s[1] - '1' < 9u
+            && s[2] == '.'
+            && s[3] - '1' < 5u
+            && s[4] == '.'
+            && s[5] - '0' < 7u)
+    {
+        r->type = WEEKDAY;
+        r->u.para[0] = s[1] - '0';
+        r->u.para[1] = s[3] - '0';
+        r->u.para[2] = s[5] - '0';
+        endp = (char *)s + 6;
+    } else if (*s == 'M'
+            && s[1] == '1'
+            && s[2] - '0' < 3u
+            && s[3] == '.'
+            && s[4] - '1' < 5u
+            && s[5] == '.'
+            && s[6] - '0' < 7u)
+    {
+        r->type = WEEKDAY;
+        r->u.para[0] = s[2] - '0' + 10;
+        r->u.para[1] = s[4] - '0';
+        r->u.para[2] = s[6] - '0';
+        endp = (char *)s + 7;
+    } else return 0;
+    s = endp;
+    r->time = 7200;
+    if (*s == '/')
+        s = getoff(s + 1, &r->time);
+    return s;
+}
+
+static int is_posix_form(const char *tz)
+{
+    if (*tz == ':') return 0;
+    char dummy[TZNAME_MAX + 1];
+    tz = getname(tz, dummy, sizeof dummy);
+
+}
+
+static int is_tz_acceptable(const char *tz)
+{
+    return !__elevated || strcmp(tz, "/etc/localtime") == 0 || 
+}
+
+static int parse_posix_form(const char *s)
+{
+    s = getname(s, tzbuf[0], TZNAME_MAX + 1);
+    if (!s) return -1;
+    s = getoff(s, &__timezone);
+    if (!s) return -1;
+    __tzname[0] = tzbuf[0];
+    if (!*s) {
+        __daylight = 0;
+        return 0;
+    }
+    s = getname(s, tzbuf[1], TZNAME_MAX + 1);
+    if (!s) return 0;   /* implementation definition: if no rule is given, dst is ignored (std is observed the whole year) */
+    __tzname[1] = tzbuf[1];
+    const char *p = getoff(s, &dstzone);
+    if (!p)
+        dstzone = __timezone - 3600;
+    else s = p;
+    if (*s != ',') return 0;
+    s = getrule(s + 1, &rules[0]);
+    if (!s || *s != ',') return 0;
+    s = getrule(s + 1, &rules[1]);
+    if (s)
+        __daylight = 1;
+    return 0;
+}
 
 static void do_tzset(void)
 {
@@ -31,7 +170,7 @@ static void do_tzset(void)
     static char *oldtz = varbuf;
     static size_t oldtzsize = sizeof varbuf;
 
-    char *tz = getenv("TZ");
+    const char *tz = getenv("TZ");
     if (!tz) tz = "/etc/localtime";
     if (!*tz) tz = utc;
     if (!strcmp(tz, oldtz)) return;
@@ -60,28 +199,45 @@ static void do_tzset(void)
     if (!is_posix_form(tz)) {
         if (*tz == ':') tz++;
         if (is_tz_acceptable(tz)) {
-            static const char *const searchpath[] = { "/usr/share/zoneinfo/", "/share/zoneinfo/", "/etc/zoneinfo/" };
-            for (size_t i = 0; i < sizeof searchpath / sizeof *searchpath; i++)
-            {
-                size_t spl = strlen(searchpath[i]);
-                char pathbuf[spl + tzlen + 1];
-                memcpy(pathbuf, searchpath[i], spl);
-                memcpy(pathbuf + spl, tz, tzlen + 1);
-                int fd = __sys_open(pathbuf, O_RDONLY);
-                if (fd != -1) {
-                    struct stat st;
-                    void *p;
-                    if (!fstat(fd, &st) && st.st_size < PTRDIFF_MAX && (p = mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0)) != MAP_FAILED) {
-                        infofile = p;
-                        infosize = st.st_size;
-                    }
-                    __syscall(SYS_close, fd);
-                    if (infofile) break;
+            int fd = -1;
+            if (tz[0] == '/')
+                fd = __sys_open(tz, O_RDONLY);
+            else {
+                static const char *const searchpath[] = { "/usr/share/zoneinfo/", "/share/zoneinfo/", "/etc/zoneinfo/" };
+                for (size_t i = 0; fd == -1 && i < sizeof searchpath / sizeof *searchpath; i++)
+                {
+                    size_t spl = strlen(searchpath[i]);
+                    char pathbuf[spl + tzlen + 1];
+                    memcpy(pathbuf, searchpath[i], spl);
+                    memcpy(pathbuf + spl, tz, tzlen + 1);
+                    fd = __sys_open(pathbuf, O_RDONLY);
                 }
             }
-            if (infofile) {
+            struct stat st;
+            char magic[4];
+            if (fd != -1
+                    && read(fd, magic, 4) == 4
+                    && !memcmp(magic, "TZif", 4)
+                    && !fstat(fd, &st)
+                    && st.st_size < PTRDIFF_MAX
+                    && (p = mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0)) != MAP_FAILED) {
+                infofile = p;
+                infosize = st.st_size;
+                tz = 0;
+                if (((char *)infofile)[infosize - 1] == '\n') {
+                    tz = (char *)infofile + infosize - 2;
+                    while (*tz != '\n') tz--;
+                    tz++;
+                }
             }
+            if (fd != -1)
+                __syscall(SYS_close, fd);
         }
+    }
+    if (tz && parse_posix_form(tz)) {
+        __daylight = 0;
+        __tzname[0] = utc;
+        __timezone = 0;
     }
 }
 
@@ -93,13 +249,82 @@ static void __tzset(void)
 }
 weak_alias(tzset, __tzset);
 
-/*
-struct tz {
-    int gmtoff;
-    int isdst;
-    int oppoff;
-    const char *name;
-    const char *oppname;
-};
-*/
-extern hidden struct tz __time_to_tz(long long, int);
+static int num_days_in_month(int mon, int isleap)
+{
+    if (mon == 2) return 28 + isleap;
+    return 30 + ((0xad5 >> (mon - 1)) & 1);
+}
+static time_t __rule_to_time(const struct rule *r, time_t yearstart, int isleap)
+{
+    if (r->type == GREGORIAN)
+        return yearstart + r->u.day * 86400;
+    if (r->type == JULIAN)
+        return yearstart + (r->u.day - 1 + (isleap && r->u.day > 59)) * 86400;
+    int daynum = yearstart / 86400;
+    static const unsigned short monthstart[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+    int firstof = daynum + monthstart[r->u.para[0] - 1];
+    int firstdowof = (r->u.para[2] - firstof - 4) % 7;
+    if (firstdowof < 0) firstdowof += 7;
+    int wantedday = firstdowof + (r->u.para[1] - 1) * 7;
+    if (wantedday >= num_days_in_month(r->u.para[0], isleap))
+        wantedday -= 7;
+    return (firstof + wantedday) * 86400 + r->time;
+}
+
+static void time_to_tz_unlocked(long long time, int islocal, struct tz *tz)
+{
+    do_tzset();
+    if (infofile && time < maxtime)
+    {
+        int i = find_trans(time, islocal);
+        /* ... */
+    } else {
+        if (__daylight) {
+            long long year = __time_to_year(islocal? time + __timezone : time);
+            int isleap;
+            time_t yearstart = __year_to_time(year, &isleap);
+            time_t t0 = __rule_to_time(&rules[0], yearstart, isleap);
+            time_t t1 = __rule_to_time(&rules[1], yearstart, isleap);
+            if (islocal) {
+                t0 -= __timezone;
+                t1 -= dstzone;
+            }
+            if (t0 < t1) {
+                if (time < t0 || time >= t1) goto std;
+                else goto dst;
+            } else {
+                if (time < t1 || time >= t0) goto dst;
+                else goto std;
+            }
+std:
+            tz->gmtoff = __timezone;
+            tz->isdst = 0;
+            tz->oppoff = dstzone;
+            tz->name = __tzname[0];
+            tz->oppname = __tzname[1];
+            return;
+
+dst:
+            tz->gmtoff = dstzone;
+            tz->isdst = 1;
+            tz->oppoff = __timezone;
+            tz->name = __tzname[1];
+            tz->oppname = __tzname[0];
+        } else {
+            tz->gmtoff = __timezone;
+            tz->isdst = 0;
+            tz->oppoff = __timezone;
+            tz->name = __tzname[0];
+            tz->oppname = __tzname[0];
+        }
+    }
+}
+
+hidden struct tz __time_to_tz(long long time, int islocal)
+{
+    struct tz tz;
+    __lock(&lock);
+    time_to_tz_unlocked(time, local, &tz);
+    __unlock(&lock);
+    return tz;
+}
