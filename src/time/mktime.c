8c1453608617a77356519351973ef1acbea89c1e
Markus Wichmann <nullplan@gmx.net>
2025-01-13 20:30:47 +0100

    Fix logic error.
    Right, the timezone offset in POSIX is the wrong way around, and I have
    to add the offset to local time to get UTC.

--- src/time/mktime.c
+++ src/time/mktime.c
@@ -5,9 +5,9 @@ time_t mktime(struct tm *tm)
     time_t rv = __tm_to_time(tm);
     struct tz tz = __time_to_tz(rv, 1);
     if (tm->tm_isdst < 0 || !tm->tm_isdst == !tz.isdst)
-        rv -= tz.gmtoff;
+        rv += tz.gmtoff;
     else
-        rv -= tz.oppoff;
+        rv += tz.oppoff;
     tz = __time_to_tz(rv, 0);
     __time_to_tm(tm, rv, &tz);
     return rv;
