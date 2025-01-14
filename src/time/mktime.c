316253173e8ae1e8cbfe1d5a11b80b82062882c8
Markus Wichmann <nullplan@gmx.net>
2025-01-14 12:55:07 +0100

    Correct mktime() timezone output.
    My god, this gets complicated fast. But yes, in the end POSIX requires
    that mktime() set the fields of the argument to the same values
    localtime() would set given the result. This means if a time far into
    the summer is given with tm_isdst=0, it is adjusted by the DST offset
    and then a time with tm_isdst=1 is returned.
    
    I am stretching the definition of "DST offset" somewhat, as with tzinfo
    files there may be more than one DST or non-DST zone defined. In this
    case, I always look up the zone the given time would be in as a local
    time, and if that zone disagrees with the wishes of the caller, I adjust
    it to the "standard" time zone (meaning the one given by the TZ string
    at the end of the file) and then adjust the output accordingly.

--- src/time/mktime.c
+++ src/time/mktime.c
@@ -5,12 +5,16 @@ time_t mktime(struct tm *tm)
     time_t local = __tm_to_time(tm);
     struct tz tz;
     tz = __time_to_tz(local, 1);
-    if (tm->tm_isdst >= 0 && !tm->tm_isdst != !tz.isdst) {
+    if (tm->tm_isdst >= 0 && !!tm->tm_isdst != tz.isdst) {
+        struct tz wanted_tz;
         if (!tm->tm_isdst)
-            tz = __get_std_tz();
+            wanted_tz = __get_std_tz();
         else
-            tz = __get_dst_tz();
+            wanted_tz = __get_dst_tz();
+        local += wanted_tz.gmtoff - tz.gmtoff;
+        tz = __time_to_tz(local, 1);
     }
-    __time_to_tm(tm, local, &tz);
+    if (__time_to_tm(tm, local, &tz))
+        return -1;
     return local + tz.gmtoff;
 }
