f5cfedb94459fb9e6e485d341f127fb8208dea3c
Markus Wichmann <nullplan@gmx.net>
2025-02-03 15:01:41 +0100

    Handle preinit array.
    There is little point to the preinitializer array in static linking, but
    it might still be in use anyway.

--- src/init/static_run_constructors.c
+++ src/init/static_run_constructors.c
@@ -6,11 +6,19 @@ typedef void (*initfunc_t)(void);
 
 extern weak initfunc_t __init_array_start[];
 extern weak initfunc_t __init_array_end[];
+extern weak initfunc_t __preinit_array_start[];
+extern weak initfunc_t __preinit_array_end[];
+
+static void run_funcs(initfunc_t *start, initfunc_t *end)
+{
+    size_t n = ((uintptr_t)end - (uintptr_t)start)/(sizeof (initfunc_t));
+    for (initfunc_t *i = start; i < start + n; i++)
+        (*i)();
+}
 
 static void static_run_constructors(void)
 {
-    size_t n = ((uintptr_t)__init_array_end - (uintptr_t)__init_array_start)/(sizeof (initfunc_t));
-    for (initfunc_t *i = __init_array_start; i < __init_array_start + n; i++)
-        (*i)();
+    run_funcs(__preinit_array_start, __preinit_array_end);
+    run_funcs(__init_array_start, __init_array_end);
 }
 weak_alias(__run_constructors, static_run_constructors);
