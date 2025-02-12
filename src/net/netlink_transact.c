ea809a819612bc507e1199aaa5af2302cfde0ffc
Markus Wichmann <nullplan@gmx.net>
2025-02-12 09:52:33 +0100

    Add if_nametoindex().

--- src/net/netlink_transact.c
+++ src/net/netlink_transact.c
@@ -1,5 +1,6 @@
 #include "netlink.h"
 #include <sys/socket.h>
+#include <string.h>
 #include <unistd.h>
 
 hidden int __netlink_transact(int (*prepare)(struct nlmsghdr *, size_t, void *), void *prepctx, int (*handle)(const struct nlmsghdr *, void *), void *handlectx)
@@ -26,23 +27,26 @@ hidden int __netlink_transact(int (*prepare)(struct nlmsghdr *, size_t, void *),
         goto out_close;
     }
 
-    rv = recv(sk, &u, sizeof u, MSG_DONTWAIT);
-    if (rv < 0) goto out_close;
-
-    for (struct nlmsghdr *nlh = &u.nlh; NLMSG_OK(nlh, rv); nlh = NLMSG_NEXT(nlh, rv))
+    for (;;)
     {
-        if (nlh->nlmsg_type == NLMSG_ERROR) {
-            struct nlmsgerr *err = NLMSG_DATA(nlh);
-            rv = err->error;
-            goto out_close;
+        rv = recv(sk, &u, sizeof u, MSG_DONTWAIT);
+        if (rv <= 0) goto out_close;
+
+        for (struct nlmsghdr *nlh = &u.nlh; NLMSG_OK(nlh, rv); nlh = NLMSG_NEXT(nlh, rv))
+        {
+            if (nlh->nlmsg_type == NLMSG_ERROR) {
+                struct nlmsgerr *err = NLMSG_DATA(nlh);
+                rv = err->error;
+                goto out_close;
+            }
+            int handle_res = handle(nlh, handlectx);
+            if (handle_res) {
+                rv = handle_res;
+                goto out_close;
+            }
+            if (!(nlh->nlmsg_flags & NLM_F_MULTI))
+                break;
         }
-        int handle_res = handle(nlh, handlectx);
-        if (handle_res) {
-            rv = handle_res;
-            goto out_close;
-        }
-        if (!(nlh->nlmsg_flags & NLM_F_MULTI))
-            break;
     }
 
 out_close:
