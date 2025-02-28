b96dc9b813f1e26af6507eb993f9165072467231
Markus Wichmann <nullplan@gmx.net>
2025-02-28 20:55:23 +0100

    Add fnmatch().
    Complicated function made worse by POSIX overgeneralizing. I have not
    implemented collation elements correctly, because they break an
    important invariant in the code. And nobody uses them anyway, since they
    are so badly specified.
    
    For now, collation elements are just skipped as unsupported, but
    unfortunately, they are a valid start or end point of a range in POSIX,
    and that is not implemented here. Maybe I'll look into how other libcs
    are doing that later.
    
    Anyway, the main algorithm is a recursive backtracking solver. I know,
    recursion is not nice, and O(n) stack frames (in the length of the
    pattern) can blow up badly. Maybe I'll look into a better one later. I
    don't want to copy musl outright, but they are the only place mentioning
    this "Sea of stars" algorithm, which I have so far failed to find
    descriptions of since unfortunately "Sea of stars" is also the name of a
    video game.

--- include/fnmatch.h
+++ include/fnmatch.h
@@ -5,14 +5,12 @@
 extern "C" {
 #endif
 
-enum {
-    FNM_NOMATCH,
-    FNM_PATHNAME,
-    FNM_PERIOD,
-    FNM_NOESCAPE,
-    FNM_CASEFOLD,
-    FNM_IGNORECASE,
-};
+#define FNM_NOMATCH     1
+#define FNM_PATHNAME    1
+#define FNM_PERIOD      2
+#define FNM_NOESCAPE    4
+#define FNM_CASEFOLD    8
+#define FNM_IGNORECASE  8
 
 int fnmatch(const char *, const char *, int);
 
