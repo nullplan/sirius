39c954060c5d6a325f50d9697829c971120b5f3b
Markus Wichmann <nullplan@gmx.net>
2025-04-01 10:13:41 +0200

    Add internal netdb.h skeleton.
    This shows how I think I am going to implement getaddrinfo() and the other
    functions. I see no great reason to mimic libresolv at this point. I don't think
    anything uses it, and I could not find a reason why musl switched to it, but it
    would mean having to conform to some API I don't know anything about. And so I
    shall not.

---
+++ src/include/netdb.h
@@ -0,0 +1,52 @@
+#ifndef NETDB_H
+#define NETDB_H
+
+#include "../../include/netdb.h"
+
+#include <stdint.h>
+
+/* getaddrinfo()/gethostbyname()/getservbyname() middleware */
+struct service {
+    uint16_t port;
+    uint16_t type;
+};
+
+struct address {
+    int af;
+    unsigned char addr[16]; /* if IPv4, this is the v4mapped address */
+    int scope_id;
+    int sortkey;
+};
+
+#define MAXSERVICE      2       /* at most one TCP and one UDP service, right? */
+#define MAXADDR         16      /* ought to be enough for anyone */
+extern hidden int __lookup_service(const char *, int, struct service [static MAXSERVICE]);
+extern hidden int __lookup_name(const char *, int, struct address [static MAXADDR], char *);
+extern hidden int __lookup_ipliteral(const char *, int, struct address *);
+
+/* backend stuff */
+#define MAXNS           3
+struct resolvconf {
+    struct address ns[MAXNS];
+    size_t nns;
+    char search[256];
+    int ndots;
+    int timeout;
+    int attempts;
+};
+/* always succeeds: If resolvconf cannot be opened/processed, it returns defaults. */
+extern hidden void __get_resolvconf(struct resolvconf *);
+
+#define DNS_RR_A        1
+#define DNS_RR_CNAME    5
+#define DNS_RR_PTR      12
+#define DNS_RR_AAAA     28
+extern hidden int __dns_create_query(const char *, int, unsigned char *, size_t);
+
+#define DNS_MAXRESULT   768     /* DNS response to AAAA query with maxed-out CNAME */
+extern hidden int __dns_transact(const unsigned char *const *, const size_t *, const struct resolvconf *, unsigned char *const *);
+extern hidden void __dns_process(const unsigned char *, size_t, int (*)(const unsigned char *, int, void *), void *);
+extern hidden int __dn_expand(const unsigned char *, const unsigned char *, size_t, char *, size_t);
+
+
+#endif
