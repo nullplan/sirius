c36244da639aabb0cc140f2d36d7b153804a7a9c
Markus Wichmann <nullplan@gmx.net>
2025-04-04 20:28:17 +0200

    Fix warning in getcwd().
    I finally decided to fix the recurring warning by restructuring the
    code. Now GCC can do the code folding for all I care.

--- src/fs/getcwd.c
+++ src/fs/getcwd.c
@@ -3,18 +3,26 @@
 #include <limits.h>
 #include "syscall.h"
 
-char *getcwd(char *p, size_t len)
+static char *inner_getcwd(char *p, size_t len)
 {
-    char buf[p? 1 : PATH_MAX];
-    if (!p) {
-        p = buf;
-        len = PATH_MAX;
-    }
-    if (syscall(SYS_getcwd, p, len) < 0)
+    long rv = __syscall(SYS_getcwd, p, len);
+    if (rv > -4096ul) {
+        errno = -rv;
         return 0;
+    }
     if (*p != '/') {
         errno = ENOENT;
         return 0;
     }
-    return p == buf? strdup(p) : p;
+    return p;
+}
+char *getcwd(char *p, size_t len)
+{
+    char buf[p? 1 : PATH_MAX];
+    if (!p) {
+        p = inner_getcwd(buf, PATH_MAX);
+        if (!p) return 0;
+        return strdup(p);
+    }
+    return inner_getcwd(p, len);
 }
