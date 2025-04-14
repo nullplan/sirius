395575a706ea2acc0a55ee92fc2f15cb8b0fefd8
Markus Wichmann <nullplan@gmx.net>
2025-04-14 10:53:15 +0200

    Clear EOF flag in ungetc().
    Successful ungetc() shall clear the EOF flag. Says so in the standard.

--- src/stdio/ungetc.c
+++ src/stdio/ungetc.c
@@ -6,6 +6,7 @@ int ungetc(int c, FILE *f)
     __FLOCK(f);
     if (!__toread(f) && c != EOF && f->buf - f->pos < UNGET) {
         rv = (*--(f->pos) = c);
+        f->flags &= ~F_EOF;
     }
     __FUNLOCK(f);
     return rv;
