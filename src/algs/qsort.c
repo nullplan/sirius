c2aa0f06b60870a29104c68ccb6d0a77fd228874
Markus Wichmann <nullplan@gmx.net>
2025-04-29 10:25:57 +0200

    Make qsort pedantic.
    ISO C indeed does not define a conversion between function pointers and object
    pointers, but it does define that we can convert both to integers, and most
    compilers implement the former conversion in terms of the latter. But this way
    silences a pedantic warning.

--- src/algs/qsort.c
+++ src/algs/qsort.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
+#include <stdint.h>
 
 static int wrap_cmp(const void *a, const void *b, void *c)
 {
-    int (*cmp)(const void *, const void *) = c;
+    int (*cmp)(const void *, const void *) = (int (*)(const void *, const void *))(uintptr_t)c;
     return cmp(a, b);
 }
 
 void qsort(void *b, size_t n, size_t sz, int (*cmp)(const void *, const void *))
 {
-    __qsort_r(b, n, sz, wrap_cmp, (void *)cmp);
+    __qsort_r(b, n, sz, wrap_cmp, (void *)(uintptr_t)cmp);
 }
