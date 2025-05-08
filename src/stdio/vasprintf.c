ca4d49c966d0e850afbbacf316870da312230b05
Markus Wichmann <nullplan@gmx.net>
2025-05-08 10:02:48 +0200

    vasprintf: Copy argument list.
    If an argument list is to be used twice, it must be copied. Or rather, once
    passed to a function that might call va_arg, an argument list is only good for
    calling va_end() on before reuse. So the argument list used for the first call
    to vsnprintf() must be a copy of the input list.

--- src/stdio/vasprintf.c
+++ src/stdio/vasprintf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 int vasprintf(char **restrict ps, const char *restrict fmt, va_list ap)
 {
-    int len = vsnprintf(0, 0, fmt, ap);
+    va_list ap2;
+    va_copy(ap2, ap);
+    int len = vsnprintf(0, 0, fmt, ap2);
+    va_end(ap2);
     if (len < 0) return len;
     *ps = malloc(len + 1);
     if (!*ps) return -1;
