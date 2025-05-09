e86ebf3fc5ec6ce58a1a0add71cd5591284c292a
Markus Wichmann <nullplan@gmx.net>
2025-05-09 13:37:45 +0200

    Fix ABA problem in semaphore implementation.
    The previous design was such that the counter value would be set to -1 for a
    locked semaphore with waiters present. Unfortunately, this means that no
    encoding is reserved for waiters present while the semaphore is filled.
    
    An ABA problem might come about like this:
    
    Initial: ctr = waiters = 0
    Thread A: sem_post() until before the a_cas(), then suspension (e.g. signal)
    Thread B: sem_wait(). Sets ctr = -1, waiters = 1 and sleeps
    Thread C: sem_wait(). Sets ctr = -1, waiters = 2 and sleeps
    Thread D: sem_post(). Sees ctr < 0 and wakes up B (let's say). ctr=1, waiters=2
    Thread B: Continues and sets ctr = 0, waiters = 1 and exits.
    Thread A: resumes. Sees ctr = 0 just as cached and sets ctr=1, waiters=1
    
    Thread C starves, unless Thread B eventually comes back for a sem_post().
    
    The "new" design is that the low 31 bits of the counter remain the semaphore
    counter, and the sign bit is the "waiters" bit. sem_post() and sem_trywait()
    mostly just leave the waiters bit alone, except sem_post() will wake up a thread
    if the bit is set.
    
    sem_wait() sets the waiters bit when it starts waiting and sees it unset, and
    clears it on the success path if it observes waiters==0. That way, we can encode
    the state "semaphore full with waiters present" int the counter value alone, and
    therefore synchronize that state correctly.

--- src/sem/sem_trywait.c
+++ src/sem/sem_trywait.c
@@ -1,11 +1,12 @@
 #include <semaphore.h>
+#include <limits.h>
 #include <errno.h>
 #include "cpu.h"
 
 int sem_trywait(sem_t *sem)
 {
     int val = sem->__ctr;
-    if (val > 0 && a_cas(&sem->__ctr, val, val - 1) == val)
+    if ((val & INT_MAX) && a_cas(&sem->__ctr, val, val - 1) == val)
         return 0;
     errno = EAGAIN;
     return -1;
