859b7fc67885c1e504a946131a5652b384b945ee
Markus Wichmann <nullplan@gmx.net>
2025-05-21 14:03:03 +0200

    Handle NULL in time().
    NULL is a valid input in time().

--- src/time/time.c
+++ src/time/time.c
@@ -4,6 +4,6 @@ time_t time(time_t *t)
 {
     struct timespec ts;
     if (__clock_gettime(CLOCK_REALTIME, &ts)) return -1;
-    *t = ts.tv_sec;
+    if (t) *t = ts.tv_sec;
     return ts.tv_sec;
 }
