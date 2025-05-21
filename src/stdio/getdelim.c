d145fcfc63b9cbe3e049b89937240b21647410a7
Markus Wichmann <nullplan@gmx.net>
2025-05-21 08:12:05 +0200

    Refactor getdelim().
    I have no way of telling whether getc_unlocked() failed for an error or EOF from
    the return value. I can only check the error flag afterwards. But of course the
    error flag might have already been set before, so I need to clear it first.
    
    No function except clearerr() is supposed to clear the error flag, so I have to
    save it and put it back in case of success. Thankfully the file is locked, so
    nobody can see me do this.
    
    I also refactored the reallocation stuff, so I have to write it only once.
    getdelim() is a weird function in that it can have partial success. My reading
    of the spec is that if getdelim() fails, it still returns the most recently
    allocated block. That needn't be null-terminated, but the application can still
    inspect it as a byte buffer, since it has a length limit.
    
    Maybe I should check what the competition is doing here.

--- src/stdio/getdelim.c
+++ src/stdio/getdelim.c
@@ -2,53 +2,62 @@
 #include <stdlib.h>
 #include <errno.h>
 
+struct dynstr {
+    char *buf;
+    size_t capacity;
+    size_t len;
+};
+
+static int push_char(struct dynstr *ds, char **ps, size_t *pn, int c, int islast)
+{
+    if (ds->len == ds->capacity)
+    {
+        size_t attempt = islast? ds->capacity + 1 :
+            ds->capacity < 8? 16 :
+            2 * ds->capacity;
+        char *alloc = realloc(ds->buf, attempt);
+        if (!alloc) {
+            errno = ENOMEM;
+            return -1;
+        }
+        *ps = ds->buf = alloc;
+        *pn = ds->capacity = attempt;
+    }
+    ds->buf[ds->len] = c;
+    if (!islast) ds->len++;
+    return 0;
+}
+
 static ssize_t getdelim_core(char **restrict ps, size_t *restrict pn, int delim, FILE *restrict f)
 {
     if (!ps || !pn) {
         errno = EINVAL;
-        f->flags |= F_ERR;
-        return -1;
+        goto error;
     }
 
     if (f->flags & F_EOF)
         return -1;
 
-    char *string = *ps;
-    size_t capacity = string? *pn : 0;
-    size_t len = 0;
+    struct dynstr ds = {*ps, *ps? 0 : *pn, 0};
+    int olderr = f->flags & F_ERR;
+    f->flags &= ~F_ERR;
 
     for (;;) {
         int c = getc_unlocked(f);
-        if (c == EOF && !len) return -1;
+        if (c == EOF && !ds.len) return -1;
         if (c == EOF) break;
 
-        if (len == capacity) {
-            size_t attempt = capacity < 8? 16 : 2 * capacity;
-            char *alloc = realloc(string, attempt);
-            if (!alloc) {
-                f->flags |= F_ERR;
-                errno = ENOMEM;
-                return -1;
-            }
-            *ps = string = alloc;
-            *pn = capacity = attempt;
-        }
-        string[len++] = c;
+        if (push_char(&ds, ps, pn, c, 0)) goto error;
         if (c == delim) break;
     }
-    if (len == capacity) {
-        size_t attempt = capacity + 1;
-        char *alloc = realloc(string, attempt);
-        if (!alloc) {
-            f->flags |= F_ERR;
-            errno = ENOMEM;
-            return -1;
-        }
-        *ps = string = alloc;
-        *pn = attempt;
-    }
-    string[len] = 0;
-    return len;
+    if (f->flags & F_ERR) return -1;
+    if (push_char(&ds, ps, pn, 0, 1)) goto error;
+    f->flags |= olderr;
+    return ds.len;
+
+error:
+    f->flags |= F_ERR;
+    return -1;
 }
 
 ssize_t getdelim(char **restrict ps, size_t *restrict pn, int delim, FILE *restrict f)
