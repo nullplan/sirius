9de32c327636e7f618f5816f360fa2bba3ab2139
Markus Wichmann <nullplan@gmx.net>
2025-05-23 08:38:56 +0200

    Fix warning.
    That should have been an error. C is dumb sometimes.

--- src/thread/pthread_cond_timedwait.c
+++ src/thread/pthread_cond_timedwait.c
@@ -2,5 +2,5 @@
 
 int pthread_cond_timedwait(pthread_cond_t *restrict c, pthread_mutex_t *restrict m, const struct timespec *restrict to)
 {
-    return pthread_cond_clockwait(c, m, to, c->__clk);
+    return pthread_cond_clockwait(c, m, c->__clk, to);
 }
