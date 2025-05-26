93609fcb07414677eedceafe120a428e25966dc1
Markus Wichmann <nullplan@gmx.net>
2025-05-26 12:28:16 +0200

    Fix configure.py.
    Now the code actually works.

--- configure.py
+++ configure.py
@@ -8,6 +8,7 @@ import fnmatch
 import tempfile
 import atexit
 import glob
+import signal
 
 tmpc = ""
 cc = ""
@@ -23,7 +24,7 @@ def trycpp(text, condition):
     print("Testing %s... " % text, end='')
     with open(tmpc, mode='w') as f:
         print("#if !(%s)\n#error fail\n#endif" % condition, file=f)
-    if subprocess.run([cc] + cflags + ["-c", "-o", "/dev/null"], stdout=DEVNULL, stderr=DEVNULL).returncode == 0:
+    if subprocess.run([cc] + cflags + ["-c", tmpc, "-o", "/dev/null"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0:
         print("yes")
         return True
     else:
@@ -42,9 +43,9 @@ def map_lib_obj_file(name):
     return oname
     
 def find_src(base, arch):
-    src = glob.glob(base + "/ " + arch + "/*.[csS]")
-    for directory in glob(srcdir + + "/src/*"):
-        for file in glob(directory + "/*.c"):
+    src = glob.glob(f"{base}/{arch}/*.[csS]")
+    for directory in glob.glob(base):
+        for file in glob.glob(directory + "/*.c"):
             bn=os.path.basename(file)[:-2]
             abn = directory + "/" + arch + "/" + bn
             if not os.path.exists(abn + ".c") and not os.path.exists(abn + ".s") and not os.path.exists(abn + ".S"):
@@ -53,13 +54,12 @@ def find_src(base, arch):
 
 
 if __name__ == "__main__":
-    cc = os.environ("CC")
-    ar = os.environ("AR")
-    arch = os.environ("ARCH")
-    cflags = os.environ("CFLAGS")
+    cc = os.getenv("CC")
+    ar = os.getenv("AR")
+    arch = os.getenv("ARCH")
+    cflags = os.getenv("CFLAGS", default="").split()
     do_static = True
     do_shared = True
-    if cflags != None: cflags = cflags.split()
 
     if cc == None: cc = "cc"
     if ar == None: ar = "ar"
@@ -83,7 +83,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     if arch == None or len(arch) == 0:
-        machine = subprocess.check_output([cc, "-dumpmachine"])
+        machine = subprocess.check_output([cc, "-dumpmachine"], encoding="utf-8")
         if fnmatch.fnmatch(machine, "i?86-*"): arch = "i386"
         elif fnmatch.fnmatch(machine, "x32-*"): arch = "x32"
         elif fnmatch.fnmatch(machine, "x86_64-*"): arch = "x86_64"
@@ -110,7 +110,7 @@ if __name__ == "__main__":
 
     flavor = "unknown"
     print("Trying to determine compiler flavor... ", end = '')
-    version = subprocess.check_output([cc, "-v"], stderr=subprocess.STDOUT)
+    version = subprocess.check_output([cc, "-v"], stderr=subprocess.STDOUT, encoding="utf-8")
     if "gcc version" in version: flavor = "gcc"
     elif "clang version" in version: flavor = "clang"
     print(flavor)
@@ -120,24 +120,24 @@ if __name__ == "__main__":
     # XXX: Taylor warning flags to CC flavor sensibly
     cflags += ["-Wall","-Wextra", "-Wno-sign-compare", "-Wno-unused-parameter", "-Wno-maybe-uninitialized", "-Wno-abi", "-Wno-unknown-pragmas", "-Wno-unused-but-set-variable"]
 
-    cflags += ["-std=c11", "-ffreestanding", "-ffunction-sections", "-fdata-sections", "-D_XOPEN_SOURCE", "-nostdinc", "-isystem", srcdir + "/include", "-isystem", srcdir + "/arch/" + ARCH, "-isystem", "obj/include", "-I", srcdir + "/src/include"]
+    cflags += ["-std=c11", "-ffreestanding", "-ffunction-sections", "-fdata-sections", "-D_XOPEN_SOURCE", "-nostdinc", "-isystem", f"{srcdir}/include", "-isystem", f"{srcdir}/arch/{arch}", "-isystem", "obj/include", "-I", f"{srcdir}/src/include"]
 
     src = find_src(srcdir + "/src/*", arch)
-    obj = map(map_obj_file, src)
+    obj = [map_obj_file(i) for i in src]
     dirs = list(dict.fromkeys(map(os.path.dirname, obj)))
     if pic_default: libobj = obj
-    else libobj = map(map_lib_obj_file, src)
+    else: libobj = [map_lib_obj_file(i) for i in src]
 
     if pic_default:
         crt1pic = "obj/crt1c.o"
         cflags += [ "-fPIC" ]
     else:
         crt1pic = "obj/crt1c.lo"
-    libgcc = subprocess.check_output([cc, "-print-libgcc-file-name"])
+    libgcc = subprocess.check_output([cc, "-print-libgcc-file-name"], encoding="utf-8").strip()
     with open("build.ninja", mode="w") as f:
         f.write(f'''cc = {cc}
 ar = {ar}
-cflags = {cflags}
+cflags = {' '.join(cflags)}
 
 rule cc
     command = $cc $cflags -MD -MF $out.d -c $in -o $out
@@ -172,18 +172,19 @@ build lib/crti.o: as {srcdir}/crt/crti.s
 build lib/crtn.o: as {srcdir}/crt/crtn.s
 build obj/include/alltypes.h: mkalltypes {srcdir}/arch/{arch}/alltypes.h.in {srcdir}/include/alltypes.h.in || obj/include
 build obj/rcrt1s.o: ccas {srcdir}/crt/{arch}/rcrt1s.S || obj
-'''
-        if do_static: f.write(f'''\
+''')
+        if do_static:
+            f.write(f'''
 build lib/libc.a: ar {' '.join(obj)} || lib
 build lib/crt1.o: ldr obj/crt1c.o obj/crt1s.o || lib
 build lib/rcrt1.o: ldr {crt1pic} obj/rcrt1s.o || lib
-'''
+''')
         if not pic_default: f.write(f"build obj/crt1c.lo: ccpic {srcdir}/crt/crt1c.c || obj\n")
         if do_static or pic_default: f.write(f"build obj/crt1c.o: cc {srcdir}/crt/crt1c.c || obj\n")
         if do_shared:
             f.write(f"build lib/Scrt1.o: ldr {crt1pic} obj/crt1s.o || lib\n")
             if pic_default:
-                f.write(f"build lib/libc.so: lds obj/rcrt1s.o obj/ldso.o {' '.join(obj)} || lib\n")
+                f.write(f"build lib/libc.so: lds obj/rcrt1s.o obj/ldso.o {' '.join(libobj)} || lib\n")
                 f.write(f"build obj/ldso.o: cc {srcdir}/ldso/ldso.c | obj/include/alltypes.h || obj\n")
             else:
                 f.write(f'''rule ccpic
@@ -192,7 +193,7 @@ build lib/rcrt1.o: ldr {crt1pic} obj/rcrt1s.o || lib
 
 build lib/libc.so: lds obj/rcrt1s.o obj/ldso.lo {' '.join(libobj)} || lib
 build obj/ldso.lo: ccpic {srcdir}/ldso/ldso.c | obj/include/alltypes.h || obj
-'''
+''')
         if os.path.exists(f"{srcdir}/crt/{arch}/crt1s.S"):
             f.write(f"build obj/crt1s.o: ccas {srcdir}/crt/{arch}/crt1s.S || obj\n")
         else:
@@ -205,6 +206,6 @@ build obj/ldso.lo: ccpic {srcdir}/ldso/ldso.c | obj/include/alltypes.h || obj
             if i.endswith(".c"):
                 f.write(f"build {o}: cc {i} | obj/include/alltypes.h || {d}\n")
                 if do_shared and not pic_default:
-                    f.write(f"build {o[:-2].lo: ccpic {i} | obj/include/alltypes.h || {d}\n")
+                    f.write(f"build {o[:-2]}.lo: ccpic {i} | obj/include/alltypes.h || {d}\n")
             elif i.endswith(".S"): f.write(f"build {o}: ccas {i} || {d}\n")
-            elif i.endswith(".s"): f.write(f"build {o}: as [i] || {d}\n")
+            elif i.endswith(".s"): f.write(f"build {o}: as {i} || {d}\n")
