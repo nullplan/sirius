955040f2c4d9d5f3ee5527daa59db27053229e6e
Markus Wichmann <nullplan@gmx.net>
2025-05-28 13:04:32 +0200

    Limit line length in posix_spawn().
    That was just a ridiculously long prototype.

--- src/spawn/posix_spawn.c
+++ src/spawn/posix_spawn.c
@@ -1,7 +1,10 @@
 #include <spawn.h>
 #include <unistd.h>
 
-int posix_spawn(pid_t *restrict pid, const char *restrict name, const posix_spawn_file_actions_t *restrict psfa, const posix_spawnattr_t *restrict psa, char *const args[restrict], char *const env[restrict])
+int posix_spawn(pid_t *restrict pid, const char *restrict name,
+        const posix_spawn_file_actions_t *restrict psfa,
+        const posix_spawnattr_t *restrict psa,
+        char *const args[restrict], char *const env[restrict])
 {
     return __spawn(pid, name, psfa, psa, args, env, execve);
 }
