881e5a16234efb9b700332033644fe29c96ba208
Markus Wichmann <nullplan@gmx.net>
2025-05-28 13:05:03 +0200

    Add system().
    This is a pretty standard by-the-books sort of system() implementation using
    posix_spawn(). One question still open (to me) is what I should do about
    cancellation here. system() is not on the list of allowed cancel points, but
    neither musl nor the example in POSIX handle the case. Not sure how this works.
    I'll just keep it the way it is currently.

---
+++ src/procs/system.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <spawn.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include "libc.h"
+
+int system(const char *cmd)
+{
+    if (!cmd) return 1;
+
+    struct sigaction ign = {.sa_handler = SIG_IGN};
+    struct sigaction oldint, oldquit;
+    sigset_t ss, restore, reset;
+
+    sigaction(SIGINT, &ign, &oldint);
+    sigaction(SIGQUIT, &ign, &oldquit);
+    sigemptyset(&ss);
+    sigaddset(&ss, SIGCHLD);
+    pthread_sigmask(SIG_BLOCK, &ss, &restore);
+
+    posix_spawnattr_t spawnattr;
+    posix_spawnattr_init(&spawnattr);
+    sigemptyset(&reset);
+    if (oldint.sa_handler != SIG_IGN) sigaddset(&reset, SIGINT);
+    if (oldquit.sa_handler != SIG_IGN) sigaddset(&reset, SIGQUIT);
+
+    posix_spawnattr_setflags(&spawnattr, POSIX_SPAWN_SETSIGMASK | POSIX_SPAWN_SETSIGDEF);
+    posix_spawnattr_setsigmask(&spawnattr, &restore);
+    posix_spawnattr_setsigdefault(&spawnattr, &reset);
+    pid_t pid;
+    int rv = posix_spawn(&pid, "/bin/sh", 0, &spawnattr, (char *[]){"sh", "-c", "--", (char *)cmd, 0}, __environ);
+    int stat = -1;
+    posix_spawnattr_destroy(&spawnattr);
+
+    if (!rv) while (waitpid(pid, &stat, 0) < 0 && errno == EINTR);
+    pthread_sigmask(SIG_SETMASK, &restore, 0);
+    sigaction(SIGQUIT, &oldquit, 0);
+    sigaction(SIGINT, &oldint, 0);
+    return 0;
+}
