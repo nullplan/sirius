a99b5d64013fd0fd3f722e61a0b7d8a2660c7bf8
Markus Wichmann <nullplan@gmx.net>
2025-06-10 08:11:53 +0200

    Fix memchr().
    The spec says that it is memchr() that must convert the argument to unsigned
    char, which on there platforms just means truncation. That has to happen under
    all circumstances.

--- src/string/memchr.c
+++ src/string/memchr.c
@@ -4,6 +4,7 @@
 void *(memchr)(const void *s, int c, size_t n)
 {
     const unsigned char *us = s;
+    c &= 0xff;
 #ifdef __GNUC__
     typedef size_t __attribute__((may_alias)) word_t;
     if (n >= 2 * sizeof (size_t)) {
@@ -15,7 +16,7 @@ void *(memchr)(const void *s, int c, size_t n)
         const word_t *ws = (const void *)us;
         const size_t ones = -1ul/255;
         const size_t highs = ones << 7;
-        const size_t mask = (c & 0xff) * ones;
+        const size_t mask = c * ones;
         while (n >= sizeof (size_t)) {
             size_t w = *ws ^ mask;
             if (~w & (w-ones) & highs) break;
