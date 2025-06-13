4ed336b12e0de5020789344ac70083a2d628980c
Markus Wichmann <nullplan@gmx.net>
2025-06-13 10:50:51 +0200

    Add getopt().
    Simple POSIX getopt(). I keep track of which options have already been processed
    with a counter. Of course it is possible for the application to change optind so
    that optchar >= strlen(argv[optind]), but I really try to avoid the issue by
    resetting optchar back to 1 when I read end of arguments. This allows conforming
    applications to safely reset optind back to 1 once getopt() has returned -1.
    
    Apparently, the GNU extension of setting optind to 0 for a reset is used in the
    wild, and I support it for now. I do not support multibyte options. That is a
    musl extension that would make everything more complicated. Fewer features means
    fewer things to go wrong.
    
    Not a big fan of the huge nested if statement at the end there. I'll see if I
    can clean that up. The logic is correct, though.

---
+++ src/env/getopt.c
@@ -0,0 +1,74 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+char *optarg;
+int opterr = 1, optind = 1, optopt;
+static size_t optchar = 1;
+int getopt(int argc, char *const argv[], const char *optstr)
+{
+    if (*optstr == '+')
+        optstr++;
+    int do_msg = opterr && *optstr != ':';
+
+    /* if optind is set to zero, the behavior is unspecified!
+     * I choose to specify it as a state reset.
+     */
+    if (!optind) optind = optchar = 1;
+
+    /* but optind = 0 is not standard, so ensure optchar = 1 when we return -1 */
+    if (optind >= argc || argv[optind][0] != '-' || argv[optind][1] == 0)
+    {
+        optchar = 1;
+        return -1;
+    }
+
+    if (argv[optind][1] == '-' && argv[optind][2] == 0)
+    {
+        optind++;
+        optchar = 1;
+        return -1;
+    }
+
+    int c = argv[optind][optchar];
+
+    const char *p = 0;
+    if (c != ':')
+        p = strchr(optstr, c);
+
+    if (p && p[1] == ':') {
+        /* known option with argument */
+        if (!argv[optind][optchar + 1]) {
+            /* option was last in its string */
+            optarg = argv[optind + 1];
+            optind += 2;
+            optchar = 1;
+            if (optind > argc) {
+                if (do_msg)
+                    fprintf(stderr, "%s: Missing argument for option `%c'.\n", argv[0], c);
+                optopt = c;
+                return *optstr == ':'? ':' : '?';
+            }
+        } else {
+            optarg = argv[optind] + optchar + 1;
+            optind++;
+            optchar = 1;
+        }
+    } else {
+        /* presumed single-option flag */
+        optchar++;
+        if (!argv[optind][optchar]) {
+            optind++;
+            optchar = 1;
+        }
+        if (!p) {
+            /* unknown option */
+            if (do_msg)
+                fprintf(stderr, "%s: Unknown option `%c'.\n", argv[0], c);
+            optopt = c;
+            return '?';
+        }
+    }
+    return c;
+}
