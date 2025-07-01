7695bc108c33ac5b75264c1c40758f87c0091aec
Markus Wichmann <nullplan@gmx.net>
2025-07-01 11:03:52 +0200

    Improve small stdc_leading_ones().
    a_clz is not defined for argument 0, but no input into these functions can turn
    ~x into zero. The value that can do that is out of range for the source type. On
    64-bit platforms, the same is true for stdc_leading_ones_ui(), but I don't want
    to make that function any uglier.

--- src/stdbit/stdc_leading_ones_uc.c
+++ src/stdbit/stdc_leading_ones_uc.c
@@ -3,5 +3,5 @@
 
 unsigned stdc_leading_ones_uc(unsigned char x) __unsequenced
 {
-    return x == 0xff? 8 : a_clz(~x) - 8 * (sizeof (size_t) - 1);
+    return a_clz(~x) - 8 * (sizeof (size_t) - 1);
 }
--- src/stdbit/stdc_leading_ones_us.c
+++ src/stdbit/stdc_leading_ones_us.c
@@ -3,5 +3,5 @@
 
 unsigned stdc_leading_ones_us(unsigned short x) __unsequenced
 {
-    return x == 0xffff? 16 : a_clz(~x) - 8 * (sizeof (size_t) - 2);
+    return a_clz(~x) - 8 * (sizeof (size_t) - 2);
 }
