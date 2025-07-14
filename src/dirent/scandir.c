ad9cef7829335ebbb09e1acd8cbb8f34369704c3
Markus Wichmann <nullplan@gmx.net>
2025-07-14 10:28:11 +0200

    Disable cancellation during scandir().
    Whether it is strictly conforming to expose this fact to the callbacks I don't
    rightly care. The point of this is to obviate the need to handle cancellation
    cleanup, and if the callbacks enable cancellation themselves, then they have to
    know what they are doing.
    
    More important than strict conformance is safety of the implementation, and
    having an implementation that sometimes leaks memory even if the application
    does everything right is not good. It is acceptable if the application does
    something dumb like re-enabling cancellation in the callbacks. OK, maybe later I
    add correct cancel handling here.

--- src/dirent/scandir.c
+++ src/dirent/scandir.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <limits.h>
 #include <errno.h>
+#include <pthread.h>
 
 static int wrap_cmp(const void *a, const void *b, void *c)
 {
@@ -14,9 +15,11 @@ int scandir(const char *name, struct dirent ***retbuf, int (*filter)(const struc
 {
     struct dirent **buffer = 0;
     size_t buflen = 0, capacity = 0;
+    int cs;
 
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, &cs);
     DIR *d = opendir(name);
-    if (!d) return -1;
+    if (!d) goto error;
 
     struct dirent *de;
     int saved_errno = errno;
@@ -25,38 +28,44 @@ int scandir(const char *name, struct dirent ***retbuf, int (*filter)(const struc
         if (filter && !filter(de)) continue;
         if (buflen == INT_MAX) {
             errno = EOVERFLOW;
-            goto error;
+            goto error_free;
         }
         if (buflen == capacity) {
             capacity = capacity < 11? 16 : capacity + capacity/2;
             if (capacity < buflen) {
                 errno = EOVERFLOW;
-                goto error;
+                goto error_free;
             }
             void *p = realloc(buffer, capacity * sizeof (struct dirent *));
-            if (!p) goto error;
+            if (!p) goto error_free;
             buffer = p;
         }
         buffer[buflen] = malloc(de->d_reclen);
-        if (!buffer[buflen]) goto error;
+        if (!buffer[buflen]) goto error_free;
         memcpy(buffer[buflen++], de, de->d_reclen);
     }
     int err = errno;
     closedir(d);
-    if (err) goto error;
+    if (err) {
+        errno = err;
+        goto error_free;
+    }
 
     errno = saved_errno;
     if (cmp)
         qsort_r(buffer, buflen, sizeof (struct dirent *), wrap_cmp, cmp);
     *retbuf = buffer;
+    pthread_setcancelstate(cs, 0);
     return buflen;
 
-error:
+error_free:
     err = errno;
     for (size_t i = 0; i < buflen; i++)
         free(buffer[i]);
     free(buffer);
     closedir(d);
     errno = err;
+error:
+    pthread_setcancelstate(cs, 0);
     return -1;
 }
