0c4abffa695089e9fe7344725c69fccd78099582
Markus Wichmann <nullplan@gmx.net>
2025-08-19 13:10:09 +0200

    Add XER to PPC clobber list.

--- arch/powerpc/syscall_arch.h
+++ arch/powerpc/syscall_arch.h
@@ -10,7 +10,7 @@ static inline long __syscall0(long nr)
     register long r0 __asm__("r0") = nr;
     register long r3 __asm__("r3");
 
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "=r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "=r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -19,7 +19,7 @@ static inline long __syscall1(long nr, long a)
     register long r0 __asm__("r0") = nr;
     register long r3 __asm__("r3") = a;
 
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -29,7 +29,7 @@ static inline long __syscall2(long nr, long a, long b)
     register long r3 __asm__("r3") = a;
     register long r4 __asm__("r4") = b;
 
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4) :: "r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4) :: "r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -40,7 +40,7 @@ static inline long __syscall3(long nr, long a, long b, long c)
     register long r4 __asm__("r4") = b;
     register long r5 __asm__("r5") = c;
 
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5) :: "r6","r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5) :: "r6","r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -52,7 +52,7 @@ static inline long __syscall4(long nr, long a, long b, long c, long d)
     register long r5 __asm__("r5") = c;
     register long r6 __asm__("r6") = d;
 
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6) :: "r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6) :: "r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -65,7 +65,7 @@ static inline long __syscall5(long nr, long a, long b, long c, long d, long e)
     register long r6 __asm__("r6") = d;
     register long r7 __asm__("r7") = e;
 
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7) :: "r8","r9","r10","r11","r12","cr0","ctr","memory");
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7) :: "r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -79,7 +79,7 @@ static inline long __syscall6(long nr, long a, long b, long c, long d, long e, l
     register long r7 __asm__("r7") = e;
     register long r8 __asm__("r8") = f;
 
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7), "+r"(r8) :: "r9","r10","r11","r12","cr0","ctr","memory");
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7), "+r"(r8) :: "r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
