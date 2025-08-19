be58c318209e4db9d63632a467cdaca6e27c4773
Markus Wichmann <nullplan@gmx.net>
2025-08-19 13:09:36 +0200

    Remove scv support again.
    It is too much stuff for too little gain. Just get rid of it.

--- arch/powerpc64/syscall_arch.h
+++ arch/powerpc64/syscall_arch.h
@@ -1,16 +1,9 @@
-#include <pthread.h>
-
 static inline long __syscall0(long nr)
 {
     register long r0 __asm__("r0") = nr;
     register long r3 __asm__("r3");
 
-#ifdef __STARTUP_CODE
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "=r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
-#else
-    register long r12 __asm__("r12") = __pthread_self()->sysinfo;
-    __asm__ volatile("mtctr %1; bctrl" : "+r"(r0), "+r"(r12), "=r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","lr","cr0","ctr","memory");
-#endif
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "=r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -19,12 +12,7 @@ static inline long __syscall1(long nr, long a)
     register long r0 __asm__("r0") = nr;
     register long r3 __asm__("r3") = a;
 
-#ifdef __STARTUP_CODE
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
-#else
-    register long r12 __asm__("r12") = __pthread_self()->sysinfo;
-    __asm__ volatile("mtctr %1; bctrl" : "+r"(r0), "+r"(r12), "+r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","lr","cr0","ctr","memory");
-#endif
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3) :: "r4","r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -34,12 +22,7 @@ static inline long __syscall2(long nr, long a, long b)
     register long r3 __asm__("r3") = a;
     register long r4 __asm__("r4") = b;
 
-#ifdef __STARTUP_CODE
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4) :: "r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
-#else
-    register long r12 __asm__("r12") = __pthread_self()->sysinfo;
-    __asm__ volatile("mtctr %1; bctrl" : "+r"(r0), "+r"(r12), "+r"(r3), "+r"(r4) :: "r5","r6","r7","r8","r9","r10","r11","lr","cr0","ctr","memory");
-#endif
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4) :: "r5","r6","r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -50,12 +33,7 @@ static inline long __syscall3(long nr, long a, long b, long c)
     register long r4 __asm__("r4") = b;
     register long r5 __asm__("r5") = c;
 
-#ifdef __STARTUP_CODE
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5) :: "r6","r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
-#else
-    register long r12 __asm__("r12") = __pthread_self()->sysinfo;
-    __asm__ volatile("mtctr %1; bctrl" : "+r"(r0), "+r"(r12), "+r"(r3), "+r"(r4), "+r"(r5) :: "r6","r7","r8","r9","r10","r11","lr","cr0","ctr","memory");
-#endif
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5) :: "r6","r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -67,12 +45,7 @@ static inline long __syscall4(long nr, long a, long b, long c, long d)
     register long r5 __asm__("r5") = c;
     register long r6 __asm__("r6") = d;
 
-#ifdef __STARTUP_CODE
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6) :: "r7","r8","r9","r10","r11","r12","cr0","ctr","memory");
-#else
-    register long r12 __asm__("r12") = __pthread_self()->sysinfo;
-    __asm__ volatile("mtctr %1; bctrl" : "+r"(r0), "+r"(r12), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6) :: "r7","r8","r9","r10","r11","lr","cr0","ctr","memory");
-#endif
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6) :: "r7","r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -85,12 +58,7 @@ static inline long __syscall5(long nr, long a, long b, long c, long d, long e)
     register long r6 __asm__("r6") = d;
     register long r7 __asm__("r7") = e;
 
-#ifdef __STARTUP_CODE
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7) :: "r8","r9","r10","r11","r12","cr0","ctr","memory");
-#else
-    register long r12 __asm__("r12") = __pthread_self()->sysinfo;
-    __asm__ volatile("mtctr %1; bctrl" : "+r"(r0), "+r"(r12), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7) :: "r8","r9","r10","r11","lr","cr0","ctr","memory");
-#endif
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7) :: "r8","r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
@@ -104,12 +72,7 @@ static inline long __syscall6(long nr, long a, long b, long c, long d, long e, l
     register long r7 __asm__("r7") = e;
     register long r8 __asm__("r8") = f;
 
-#ifdef __STARTUP_CODE
-    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7), "+r"(r8) :: "r9","r10","r11","r12","cr0","ctr","memory");
-#else
-    register long r12 __asm__("r12") = __pthread_self()->sysinfo;
-    __asm__ volatile("mtctr %1; bctrl" : "+r"(r0), "+r"(r12), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7), "+r"(r8) :: "r9","r10","r11","lr","cr0","ctr","memory");
-#endif
+    __asm__ volatile("sc; bns 1f; neg 3,3; 1:" : "+r"(r0), "+r"(r3), "+r"(r4), "+r"(r5), "+r"(r6), "+r"(r7), "+r"(r8) :: "r9","r10","r11","r12","cr0","ctr","xer","memory");
     return r3;
 }
 
