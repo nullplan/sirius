594cdb9bd7345ed9bb16fbc028adc29b5ca69d72
Markus Wichmann <nullplan@gmx.net>
2025-09-02 13:38:46 +0200

    Simplify VDSO stuff.
    The vdso is not supposed to contain too many symbols, so even having the hash
    function is kind of overkill. Far simpler to just iterate over the symbols in
    order. Thankfully the SysV hash table provides the number of symbols inside.

--- src/init/vdso.c
+++ src/init/vdso.c
@@ -20,21 +20,10 @@ typedef Elf32_Verdaux Verdaux; /* same type either way */
 
 static const char *strtab;
 static const Sym *symtab;
-static const uint32_t *hashtab;
 static const Verdef *verdef;
 static const uint16_t *versym;
 static size_t base;
-
-static uint32_t elf_hash(const char *name)
-{
-    const unsigned char *u = (void *)name;
-    uint_fast32_t h = 0;
-    while (*u) {
-        h = (h << 4) + *u++;
-        h ^= (h >> 24) & 0xf0;
-    }
-    return h & 0x0fffffff;
-}
+static size_t numsyms;
 
 hidden void __init_vdso(const void *p)
 {
@@ -43,6 +32,7 @@ hidden void __init_vdso(const void *p)
     const Phdr *ph0 = (void *)((const char *)p + eh->e_phoff);
     const Phdr *ph = ph0;
     const size_t *dyn = 0;
+    const uint32_t *hashtab = 0;
     size_t phnum = eh->e_phnum, phent = eh->e_phentsize;
     for (; phnum; phnum--, ph = (void *)((const char *)ph + phent)) {
         if (ph->p_type == PT_DYNAMIC) {
@@ -63,9 +53,10 @@ hidden void __init_vdso(const void *p)
         }
     }
     if (!versym) verdef = 0;
+    if (hashtab) numsyms = hashtab[1];
 }
 
-static int match_version(uint16_t versym, const char *ver, uint32_t hash)
+static int match_version(uint16_t versym, const char *ver)
 {
     versym &= 0x7fff;
     const Verdef *def = verdef;
@@ -78,26 +69,21 @@ static int match_version(uint16_t versym, const char *ver, uint32_t hash)
         def = (void *)((char *)def + def->vd_next);
     }
     Verdaux *va = (void *)((char *)def + def->vd_aux);
-    return def->vd_hash == hash && !strcmp(ver, strtab + va->vda_name);
+    return !strcmp(ver, strtab + va->vda_name);
 }
 #define OK_BINDS (1 << STB_GLOBAL | 1 << STB_WEAK)
 #define OK_TYPES (1 << STT_FUNC)
 hidden void *__vdsosym(const char *name, const char *ver)
 {
-    if (!strtab || !symtab || !hashtab) return 0;
-    uint32_t h = elf_hash(name);
-    uint32_t vh = 0;
-    const uint32_t *bucket = hashtab + 2;
-    const uint32_t *chain = bucket + hashtab[0];
-    if (verdef) vh = elf_hash(ver);
-    for (uint32_t idx = bucket[h % hashtab[0]]; idx; idx = chain[idx])
+    if (!strtab || !symtab) return 0;
+    for (uint32_t idx = 1; idx < numsyms; idx++)
     {
         const Sym *sym = symtab + idx;
         if (((1 << ST_BIND(sym->st_info)) & OK_BINDS)
                 && (1 << ST_TYPE(sym->st_info) & OK_TYPES)
                 && sym->st_shndx != SHN_UNDEF
                 && !strcmp(name, strtab + sym->st_name)
-                && (!verdef || match_version(versym[idx], ver, vh)))
+                && (!verdef || match_version(versym[idx], ver)))
             return (void *)(base + sym->st_value);
     }
     return 0;
