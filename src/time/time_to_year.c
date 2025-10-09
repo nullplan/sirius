820b0bb6733dfafc27af73cd0cc7baf660d0f82a
Markus Wichmann <nullplan@gmx.net>
2025-10-09 10:43:30 +0200

    Add explanation for divisor.
    This is the only place in the code where we use that number, and completely
    without explanation it is a bit baffling.

--- src/time/time_to_year.c
+++ src/time/time_to_year.c
@@ -1,4 +1,8 @@
 #include <time.h>
+/* taking all rules of the Gregorian calendar together,
+ * the year has on average 365.2425 days.
+ * 365.2425 * 86400 = 31,556,952
+ */
 hidden long long __time_to_year(time_t t)
 {
     long long y = t / 31556952 + 70;
