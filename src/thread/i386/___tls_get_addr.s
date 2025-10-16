388ceb1f65798879337b2bfd1192cbc7bde81f4c
Markus Wichmann <nullplan@gmx.net>
2025-10-16 13:04:59 +0200

    Optimize ___tls_get_addr().
    I noticed I can reduce register usage and number of instructions as before on
    x86_64.

--- src/thread/i386/___tls_get_addr.s
+++ src/thread/i386/___tls_get_addr.s
@@ -2,10 +2,9 @@
 .type ___tls_get_addr, @function
 ___tls_get_addr:
     # return __pthread_self()->dtv[eax[0]] + eax[1]
-    # return (%gs:16 + eax[0]) + eax[1]
-    movl %gs:16, %edx
+    # return *(%gs:16 + eax[0]) + eax[1]
     movl (%eax), %ecx
     movl 4(%eax), %eax
-    addl (%edx, %ecx, 4), %eax
+    addl %gs:16(, %ecx, 4), %eax
     retl
 .size ___tls_get_addr, . - ___tls_get_addr
