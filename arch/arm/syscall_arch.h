3a9cc1f8ab2cc7cc3d9773899c4e50a4c9a4de31
Markus Wichmann <nullplan@gmx.net>
2025-12-09 10:38:49 +0100

    Optimize thumb syscalls.
    Reading the musl source code gave me an idea to depart from the nolibc way of
    doing Thumb syscalls. The problem is that r7 may not be available for Thumb
    inline assembly. As with i386 before, I therefore have to treat it as never
    available, since the conditions are impossible to test. r7 is used as frame
    pointer, and is needed if -fno-omit-frame-pointer is present in CFLAGS, or if
    optimization is turned off, or if the calling function (these are inline
    functions, after all) dynamically allocates stack.
    
    The solution nolibc has for this is to stash the syscall number in r6, then swap
    r6 and r7 in the leadup to the syscall, then restore r7 from r6. This solution
    works, but it inflexibly puts three unavoidable instructions in front of every
    syscall, and allocates a register more, when we have high register pressure in
    Thumb1 already.
    
    The musl solution looks somewhat more complicated, but it uses a compound
    literal to make the compiler allocate a free register. So not r6 under all
    circumstances. Also, the syscall number is not put into the temp register
    beforehand, so its value can be lost. In Thumb2 mode, r7 can be set directly
    from a literal. In Thumb1, this would work as well, except that GCC has no
    constraints to tell it to only use a literal for integers 0-255. Also, the
    instruction used would have to be "movs", not "mov".

--- arch/arm/syscall_arch.h
+++ arch/arm/syscall_arch.h
@@ -7,90 +7,89 @@
 
 /* in thumb mode, r7 is used as frame pointer, and can therefore not
  * be used in inline assembler.
- * To circumvent the issue, I put the syscall number into r6 there, then swap r6 with r7
- * in the lead-up to the syscall, then just restore r7 from r6 afterward,
- * and tell the compiler that the register gets updated.
  */
 #ifdef __thumb__
-#define __SC_NR_REG "r6"
-#define __SC_NR_CLOB ,"=r"(r7)
-#define __SC_CC_CLOB ,"cc" /* eors changes the condition codes */
-#define __SC_INSTR ".syntax unified\n\teors r7, r6; eors r6, r7; eors r7, r6; svc #0; movs r7, r6"
+#define __ASM____R7__
+#define __do_syscall(...) __asm__ volatile("mov %1, r7; mov r7, %r2; svc 0; mov r7, %1" : "=r"(r0), "=&r"((int){0}) : __VA_ARGS__ : "memory")
 #else
-#define __SC_NR_REG "r7"
-#define __SC_NR_CLOB
-#define __SC_CC_CLOB
-#define __SC_INSTR "svc #0"
+#define __ASM____R7__ __asm__("r7")
+#define __do_syscall(...) __asm__ volatile("svc 0" : "=r"(r0) : __VA_ARGS__ : "memory")
+#endif
+
+#ifdef __thumb2__
+#define __R7_OP "rI"(r7)
+#else
+#define __R7_OP "r"(r7)
 #endif
 
 static inline long __syscall0(long nr)
 {
-    register long r7 __asm__(__SC_NR_REG) = nr;
+    register long r7 __ASM____R7__ = nr;
     register long r0 __asm__("r0");
-    __asm__ volatile(__SC_INSTR : "=r"(r0) __SC_NR_CLOB : "r"(r7) : "lr", "memory" __SC_CC_CLOB);
+    __do_syscall(__R7_OP);
     return r0;
 }
 
 static inline long __syscall1(long nr, long a)
 {
-    register long r7 __asm__(__SC_NR_REG) = nr;
+    register long r7 __ASM____R7__ = nr;
     register long r0 __asm__("r0") = a;
-    __asm__ volatile(__SC_INSTR : "+r"(r0) __SC_NR_CLOB : "r"(r7) : "lr", "memory" __SC_CC_CLOB);
+    __do_syscall(__R7_OP, "r"(r0));
     return r0;
 }
 
 static inline long __syscall2(long nr, long a, long b)
 {
-    register long r7 __asm__(__SC_NR_REG) = nr;
+    register long r7 __ASM____R7__ = nr;
     register long r0 __asm__("r0") = a;
     register long r1 __asm__("r1") = b;
-    __asm__ volatile(__SC_INSTR : "+r"(r0) __SC_NR_CLOB : "r"(r7), "r"(r1) : "lr", "memory" __SC_CC_CLOB);
+    __do_syscall(__R7_OP, "r"(r0), "r"(r1));
     return r0;
 }
 
 static inline long __syscall3(long nr, long a, long b, long c)
 {
-    register long r7 __asm__(__SC_NR_REG) = nr;
+    register long r7 __ASM____R7__ = nr;
     register long r0 __asm__("r0") = a;
     register long r1 __asm__("r1") = b;
     register long r2 __asm__("r2") = c;
-    __asm__ volatile(__SC_INSTR : "+r"(r0) __SC_NR_CLOB : "r"(r7), "r"(r1), "r"(r2) : "lr", "memory" __SC_CC_CLOB);
+    __do_syscall(__R7_OP, "r"(r0), "r"(r1), "r"(r2));
     return r0;
 }
 
 static inline long __syscall4(long nr, long a, long b, long c, long d)
 {
-    register long r7 __asm__(__SC_NR_REG) = nr;
+    register long r7 __ASM____R7__ = nr;
     register long r0 __asm__("r0") = a;
     register long r1 __asm__("r1") = b;
     register long r2 __asm__("r2") = c;
     register long r3 __asm__("r3") = d;
-    __asm__ volatile(__SC_INSTR : "+r"(r0) __SC_NR_CLOB : "r"(r7), "r"(r1), "r"(r2), "r"(r3) : "lr", "memory" __SC_CC_CLOB);
+    __do_syscall(__R7_OP, "r"(r0), "r"(r1), "r"(r2), "r"(r3));
     return r0;
 }
 
 static inline long __syscall5(long nr, long a, long b, long c, long d, long e)
 {
-    register long r7 __asm__(__SC_NR_REG) = nr;
+    register long r7 __ASM____R7__ = nr;
     register long r0 __asm__("r0") = a;
     register long r1 __asm__("r1") = b;
     register long r2 __asm__("r2") = c;
     register long r3 __asm__("r3") = d;
     register long r4 __asm__("r4") = e;
-    __asm__ volatile(__SC_INSTR : "+r"(r0) __SC_NR_CLOB : "r"(r7), "r"(r1), "r"(r2), "r"(r3), "r"(r4) : "lr", "memory" __SC_CC_CLOB);
+    __do_syscall(__R7_OP, "r"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r4));
     return r0;
 }
 
 static inline long __syscall6(long nr, long a, long b, long c, long d, long e, long f)
 {
-    register long r7 __asm__(__SC_NR_REG) = nr;
+    register long r7 __ASM____R7__ = nr;
     register long r0 __asm__("r0") = a;
     register long r1 __asm__("r1") = b;
     register long r2 __asm__("r2") = c;
     register long r3 __asm__("r3") = d;
     register long r4 __asm__("r4") = e;
     register long r5 __asm__("r5") = f;
-    __asm__ volatile(__SC_INSTR : "+r"(r0) __SC_NR_CLOB : "r"(r7), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5) : "lr", "memory" __SC_CC_CLOB);
+    __do_syscall(__R7_OP, "r"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5));
     return r0;
 }
 
